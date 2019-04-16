2bcbbde5e333c9fb14fa14379f445d09be3ff237
praathul <44019979+athulpram@users.noreply.github.com>
2019-04-16 08:59:27 +0530

    Updates

---
+++ precache-manifest.6e8281b7867ef78d5602501f2d409b71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "79cfff512961ec779926",
+    "url": "/solitare/static/js/runtime~main.cd64d29b.js"
+  },
+  {
+    "revision": "b3beae39134a5804a738",
+    "url": "/solitare/static/js/main.14f8f790.chunk.js"
+  },
+  {
+    "revision": "a324ed79526a599ad833",
+    "url": "/solitare/static/js/2.bd58e6ba.chunk.js"
+  },
+  {
+    "revision": "b71bbb3347c62154ff35c6f49500210b",
+    "url": "/solitare/index.html"
+  }
+];
